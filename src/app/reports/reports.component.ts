1829e7f62b75de2774a5639bd465ec2ef4b4aa5b
javiermartinust <javier.martin1@ust-global.com>
2020-04-08 12:26:02 +0200

    added dialogs to reports view and amended actions and list of it to be same as decisions view

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import {
 } from "@angular/forms";
+import { MatDialog } from '@angular/material/dialog';
+import { UploadDialogComponent } from "../shared/component/dialog/dialog.component";
+import { SearchDialogComponent } from "../shared/component/dialog/dialog.component";
+import { FilterDialogComponent } from "../shared/component/dialog/dialog.component";
+import { SortDialogComponent } from "../shared/component/dialog/dialog.component";
 
 @Component({
   selector: "app-reports",
@@ -10,7 +15,48 @@ import {
 
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(public dialog: MatDialog) { }
+
+  openUploadDialog(): void {
+    const dialogRef = this.dialog.open(UploadDialogComponent, {
+      width: '500px',
+      height: '300px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  openSearchDialog(): void {
+    const dialogRef = this.dialog.open(SearchDialogComponent, {
+      width: '500px',
+      height: '300px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  openFilterDialog(): void {
+    const dialogRef = this.dialog.open(FilterDialogComponent, {
+      width: '500px',
+      height: '300px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  openSortDialog(): void {
+    const dialogRef = this.dialog.open(SortDialogComponent, {
+      width: '500px',
+      height: '300px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 
   async ngOnInit() {
   }
