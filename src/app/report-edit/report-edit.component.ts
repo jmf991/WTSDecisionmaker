9fa9f469fde3c14690131c127d82c977ce2d3490
javiermartinust <javier.martin1@ust-global.com>
2020-04-14 17:17:08 +0200

    added upload button to detail views, css amends for detail views

--- src/app/report-edit/report-edit.component.ts
+++ src/app/report-edit/report-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import {
 } from "@angular/forms";
+import { MatDialog } from '@angular/material/dialog';
+import { UploadDialogComponent } from "../shared/component/dialog/dialog.component";
 
 @Component({
   selector: "app-report-edit",
@@ -10,10 +12,18 @@ import {
 
 export class ReportEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   async ngOnInit() {
   }
-
+  openUploadDialog(): void {
+    const dialogRef = this.dialog.open(UploadDialogComponent, {
+      width: '500px',
+      height: '300px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 
 }
