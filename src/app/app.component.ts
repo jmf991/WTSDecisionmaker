3c48c95fc94f0b169f4496cd3a8188491a581380
javiermartinust <javier.martin1@ust-global.com>
2020-11-13 08:57:56 +0100

    Report details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CdkColumnDef } from '@angular/cdk/table';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[CdkColumnDef]
 })
 export class AppComponent {
   title = 'WTS-decisionmaker';
--- src/app/report-details/report-details.component.ts
+++ src/app/report-details/report-details.component.ts
@@ -11,8 +11,11 @@ import {
 export class ReportDetailsComponent implements OnInit {
 
   constructor() { }
+  displayedColumns: string[] = ['Question', 'Answer'];
+  dataSource: any;
 
   async ngOnInit() {
+    this.dataSource = [{ "id": 0, "question": "Ist innerhalb der letzten drei  Monate bereits eine Bewirtung  an diese Personen erfolgt?", "answer": "Ja", "response": "Eine Bewirtung ist unter diesen Umständen nicht erlaubt", "date": "2020-11-11T13:21:36", "final": true }]
   }
 
 
